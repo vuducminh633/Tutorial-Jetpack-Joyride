6c312335b20afdb3c3f07cd3286644d1c0e1faca
vuducminh633 <vuducminh633@gmail.com>
2025-11-13 15:15:21 +0700

    Unity UI – Part 2 Done

--- Assets/Script/UIManager.cs
+++ Assets/Script/UIManager.cs
@@ -2,11 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+    public Animator startBtn;
+    public Animator settingBtn;
+    public Animator settignDialog;
+
+
     public void StartGame()
     {
         SceneManager.LoadScene("RocketMouse");
+    }
+
+    public void OpenSetting()
+    {
+        startBtn.SetBool("isHidden", true);
+        settingBtn.SetBool("isHidden", true);
+
+        settignDialog.SetBool("isHidden", false);
+    }
+
+    public void CloseSetting()
+    {
+        startBtn.SetBool("isHidden", false);
+        settingBtn.SetBool("isHidden", false);
+
+        settignDialog.SetBool("isHidden", true);
     }
 }
